9d3776c8d10e4e4555dd16ddf8cec493fc239bcf
瞿龙俊 <154256698@qq.com>
2015-12-14 13:19:34 +0800

    day4

--- SimpleProject/js/apps.js
+++ SimpleProject/js/apps.js
@@ -18,6 +18,7 @@
 			bold: 'normal',
 			italic: 'normal',
 			textAlign: 'left',
+			picSrc:'',
 		}
 	});
 
@@ -53,19 +54,32 @@
 		el: $("#main-Context"),
 		initialize: function() {
 			this.collection.bind("add", this.render);
+			this.collection.bind("remove", this.removeEle);
 		},
 		events: {
 			"click .box": 'clickBox',
 			"change #eleName": "eleNameChange",
-			"change #valueEle": "valueEleChange"
+			"change #valueEle": "valueEleChange",
+			"click .deleteEle": "deleteEle",
+			"click #changeBtn":"picSrc"
 		},
 		render: function(model) {
 			//bounceInLeft
-			var newDiv = $('<li><div id="' + model.cid + '" class="box animated "><' + model.get('tag') + '>' + model.get('value') + '</' + model.get('tag') + '></div></li>')
+			var newDiv = "";
+			if (model.get('tag') == "IMG") {
+				 newDiv = $('<li><div id="' + model.cid + '" class="box animated" style="height: 500px;"><' + model.get('tag') + ' src="img/备选图片.jpg" style="width:100%;heigth:100%" /></div></li>')
+			} else {
+				 newDiv = $('<li><div id="' + model.cid + '" class="box animated "><' + model.get('tag') + '>' + model.get('value') + '</' + model.get('tag') + '></div></li>')
+			}
 			$("#main-Panel").append(newDiv);
 			$('#eleName').prop("value", model.get("name"));
 			$('#valueEle').prop('value', model.get("value"));
 			return this;
+		},
+		removeEle: function(model) {
+			$('#' + model.cid).parents('li').remove();
+			$('#eleName').prop("value", "");
+			$('#valueEle').prop("value", "");
 		},
 		clickBox: function(event) {
 			var ele = event.target.tagName == "DIV" ? event.target : event.target.parentElement;
@@ -73,11 +87,10 @@
 			var model = chooseList.getByCid(cid);
 			$('.box').removeClass('chooseItemChecked');
 			ele.className = "box  chooseItemChecked";
-			var temp=this.collection.getByCid(cid)
-			$('#valueEle').prop("value",temp.get("value"));
-			$('#eleName').prop("value",temp.get("name"));
-			chooseView.model=temp;
-			
+			var temp = this.collection.getByCid(cid)
+			$('#valueEle').prop("value", temp.get("value"));
+			$('#eleName').prop("value", temp.get("name"));
+			chooseView.model = temp;
 		},
 		eleNameChange: function(event) {
 			this.model.set("name", $('#eleName').prop('value'));
@@ -86,6 +99,19 @@
 			var value = $('#valueEle').prop('value')
 			this.model.set("value", value);
 			$('#' + this.model.cid).children().html(value);
+		},
+		picSrc:function(event){
+			var path=$('#changePic').val();
+			alert(path);
+			$('#' + this.model.cid).find('IMG').attr('src',path);
+		},
+		deleteEle: function(event) {
+
+			if (this.model == undefined) {
+				alert("画布中至少需要一个元素！");
+			} else {
+				this.collection.remove(this.model);
+			}
 		}
 	});
 	var chooseList = new ChooseTextItemList();
@@ -93,4 +119,8 @@
 	var chooseView = new ChooseView({
 		collection: chooseList
 	});
+	$('#changeBtn').click(function(){
+		//alert($('#changePic').val());
+		
+	})
 })();
